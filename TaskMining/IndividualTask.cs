3b431ff6b18c8586eff4ce951483465df241598f
Thomas Taulo <taulo@live.dk>
2022-11-03 13:55:28 +0100

    remove unnecessary methods

--- TaskMining/IndividualTask.cs
+++ TaskMining/IndividualTask.cs
@@ -21,40 +21,14 @@ namespace TaskMining
             UserName = userName;
             ApplicationName = applicationName;
             Data = data;
-        }  
-
+        }
         public DateTime GetDateTime()
         {
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dateTime = dateTime.AddSeconds(double.Parse(TimeStamp) / 1000);
             return dateTime;
         }
-
-        public DateTime GetDateTime(double ts)
-        {
-            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddSeconds(ts / 1000);
-            return dateTime;
-        }
-
-        // calculate total time a task has been completed in seconds
-        public double TotalTaskCompletionTimeInSeconds(IndividualTask wtStart, IndividualTask wtEnd)
-        {
-            double ts = double.Parse(wtEnd.TimeStamp) - double.Parse(wtStart.TimeStamp);
-            return ts;
-        }
-        public DateTime TotalTaskCompletionTime(IndividualTask wtStart, IndividualTask wtEnd)
-        {
-            double subtractTs = TotalTaskCompletionTimeInSeconds(wtStart, wtEnd);
-            long uxNow = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
             
-            DateTime time = GetDateTime(uxNow - subtractTs);
-
-            Console.WriteLine(time);
-
-            return new DateTime(time.Year, time.Month, time.Day, time.Hour, time.Minute, time.Second);
-        }
-
         override
         public string ToString()
         {

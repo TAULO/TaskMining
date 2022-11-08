36222618693baa944a3981be5f7737ededaac8e8
Thomas Taulo <taulo@live.dk>
2022-11-08 13:44:39 +0100

    add test

---
+++ TaskMiningTest/IndividualTaskUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskMiningTest
+{
+    [TestClass]
+    public class IndividualTaskUnitTest
+    {
+        [TestMethod]
+        public void TestGetDateTime()
+        {
+            IndividualTask task = new IndividualTask();
+        }
+    }
+}
